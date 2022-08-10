f15407d87125ffb2afe2adb46c3dd72f93b6a9e7
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2022-08-10 19:31:48 +0300

    .

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-var cors = require('cors')
-const app = express();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const APP_PORT = 6006;
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-app.options('*', cors());
+var app = express();
 
-app.use(bodyParser.json())
-app.get('/', (req, res) => {
-  res.send("Hello, world!")
-})
-app.post(`/seobility`,(req, res) =>{
-  if(req.body.phone === "+7 (___) ___-__-__") {
-    res.status(400).send({status:"error", message: "Enter correct phone"})
-    return
-  }
-  res.status(200).send({status:"success", message:"Successful sending"})
-})
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.listen(APP_PORT, () => {
-  console.warn(`App listening on port ${APP_PORT}`)
-})
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
